c066c1650eec1f3fcfbcaa8d64a046202e396d34
Sergey Vasilenko <vasilenko.sn@gmail.com>
2013-02-23 13:31:57 +0400

    Init application

---
+++ source/src/ru/sergeyvasilenko/weheartpics/interview/PhotoListActivity.java
@@ -0,0 +1,15 @@
+package ru.sergeyvasilenko.weheartpics.interview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhotoListActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
